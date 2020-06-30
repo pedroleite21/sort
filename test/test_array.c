3b0ba07eb165f237c5b7022ef56d352cd2d1ecd8
pedroleite21 <pedro.leite.001@acad.pucrs.br>
2020-06-30 20:27:55 -0300

    Add test to fail

--- test/test_array.c
+++ test/test_array.c
@@ -13,6 +13,18 @@ void tearDown(void)
 
 void test_RandomArray(void)
 {
+    // TEST TO FAIL
+    int size = 5;
+    int *array = malloc(size*sizeof(int));
+    int assert_array[6] = {5, 4, 3, 2, 1};
+
+    random_array(array, size);
+
+    for (int i = 1; i <= size; i++)
+    {
+
+        TEST_ASSERT_EQUAL(assert_array[i], array[i]);
+    }
 }
 
 void test_AscendingArray(void)
@@ -31,16 +43,41 @@ void test_AscendingArray(void)
 
 void test_DescendingArray(void)
 {
+    int size = 5;
+    int *array = malloc(size*sizeof(int));
+    int assert_array[6] = {5, 4, 3, 2, 1};
+
+    descending_array(array, size);
+
+    for (int i = 1; i <= size; i++)
+    {
+
+        TEST_ASSERT_EQUAL(assert_array[i], array[i]);
+    }
 }
 
 void test_AlmostOrderedArray(void)
 {
+    // TEST TO FAIL
+    int size = 5;
+    int *array = malloc(size*sizeof(int));
+    int assert_array[6] = {5, 4, 3, 2, 1};
+
+    almost_ordered_array(array, size);
+
+    for (int i = 1; i <= size; i++)
+    {
+
+        TEST_ASSERT_EQUAL(assert_array[i], array[i]);
+    }
 }
 
 int main(void)
 {
     UNITY_BEGIN();
     RUN_TEST(test_AscendingArray);
-    // RUN_TEST(test_AverageThreeBytes_should_AverageHighValues);
+    RUN_TEST(test_DescendingArray);
+    RUN_TEST(test_AlmostOrderedArray);
+    RUN_TEST(test_RandomArray);
     return UNITY_END();
 }
